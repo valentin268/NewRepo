c0c3ed00e34e5995252c5d796eff6591097f550b
Student <Student@DESKTOP-6NP6TQ3>
2025-06-09 20:33:56 +0300

    базовая версия ToDo

--- Project1/Project1/Project1.cpp
+++ Project1/Project1/Project1.cpp
@@ -1,12 +1,24 @@
 ﻿#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
 
+vector<string> tasks;
+
+void addTask(const string& task) {
+    tasks.push_back(task);
+}
+
+void showTask() {
+
+    cout << "ToDo list: " << endl;
+    for (size_t i = 0; i < tasks.size(); i++) {
+        cout << i + 1 << ". " << tasks[i] << endl;
+    }
+}
+
 int main() {
-    int age;
-    cout << "Сколько тебе лет?\n";
-    cin >> age;
-    cout << "привет" << age;
-    //3
-    //4
-    //5
+    addTask("Go to the school");
+    addTask("Pass the exam");
+    showTask();
 }
